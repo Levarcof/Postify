e5bcd67ed9671954e468392391f1b1b568b83fe6
Vikram Prajapat <levarcof@gmail.com>
2026-03-23 16:17:38 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,28 +1,41 @@
 import nodemailer from "nodemailer";
 
+// ✅ SMTP connection test
+
+
 const sendEmail = async (email, otp) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-     tls: {
-    rejectUnauthorized: false
+  try {
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+
+    transporter.verify((error, success) => {
+      if (error) {
+        console.log("SMTP connection failed:", error);
+      } else {
+        console.log("SMTP server ready");
+      }
+    });
+
+    const info = await transporter.sendMail({
+      from: `"OTP Verification" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "OTP Verification",
+      html: `<h2>Your OTP is ${otp}</h2>`
+    });
+
+    console.log("Email sent:", info.response);
+
+  } catch (error) {
+    console.log("Email error:", error);
   }
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "OTP Verification",
-    text: `Your OTP is ${otp}`,
-  };
-
-  await transporter.sendMail(mailOptions);
 };
 
 export default sendEmail;
